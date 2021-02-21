d482774701901a995046add79931fc637452d32f
realovka <realovka@mail.ru>
2021-02-21 13:50:13 +0300

    update Start.java

--- src/main/java/by/realovka/Start.java
+++ src/main/java/by/realovka/Start.java
@@ -3,6 +3,7 @@ package by.realovka;
 
 import by.realovka.single.DietarySupplement;
 import by.realovka.single.Medicine;
+import by.realovka.single.Product;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -24,8 +25,10 @@ public class Start {
                 .packageDivision(false)
                 .belarusianProduct(false)
                 .build();
-        session.save(cerebrolizat);
-        session.save(biogaya);
+//        session.save(cerebrolizat);
+//        session.save(biogaya);
+        Product object = session.find(Product.class, 3L);
+        System.out.println(object);
         trx.commit();
         session.close();
     }
