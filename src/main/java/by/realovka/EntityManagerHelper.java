2236b539787479690f1cc539825396c48cf3a587
realovka <realovka@mail.ru>
2021-02-22 11:23:15 +0300

    add EntityManagerHelper

---
+++ src/main/java/by/realovka/EntityManagerHelper.java
@@ -0,0 +1,30 @@
+package by.realovka.web.dao.dao;
+
+import lombok.SneakyThrows;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import javax.persistence.EntityManager;
+
+public class EntityManagerHelper {
+
+    private final SessionFactory factory;
+
+    @SneakyThrows
+    private EntityManagerHelper() {
+        Configuration cfg = new Configuration().configure();
+        this.factory = cfg.buildSessionFactory();
+    }
+
+    private static class EntityManagerHelperHolder {
+        private static final EntityManagerHelper HOLDER_INSTANCE = new EntityManagerHelper();
+    }
+
+    public static EntityManagerHelper getInstance() {
+        return EntityManagerHelper.EntityManagerHelperHolder.HOLDER_INSTANCE;
+    }
+
+    public EntityManager getEntityManager() {
+        return factory.createEntityManager();
+    }
+}
